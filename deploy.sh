3ba8ec1d1365360d00c826430c60dda01991a486
Vishal T <vishal15276@gmail.com>
2025-03-20 15:05:50 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@
     sh 'chmod +x build.sh'
     sh './build.sh'
     docker login -u vishal15276t -p dckr_pat_bbHBKdl_E6eNv8VNpK6reAXYoLM
-    docker tag test vishal15276t/test1
+    docker tag test1 vishal15276t/test1
     docker push vishal152786t/test1
