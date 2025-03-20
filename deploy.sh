1ebaf48cfd2e9f6551da7e97ab8083ee81956673
Vishal T <vishal15276@gmail.com>
2025-03-20 15:08:13 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@
     sh './build.sh'
     docker login -u vishal15276t -p dckr_pat_bbHBKdl_E6eNv8VNpK6reAXYoLM
     docker tag test1 vishal15276t/test1
-    docker push vishal152786t/test1
+    docker push vishal15276t/test1
